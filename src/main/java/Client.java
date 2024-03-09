3a7e6262c646543a2c52f9a50367ac903af8b4cb
Juce <iamjuceuwu@gmail.com>
2024-03-09 13:54:38 +0000

    we can now send a frame to the connected client

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,7 +1,17 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Client {
 
     private Client connectedClient;
     private Client callingClient;
+    public int receivedFrames;
 
     public void call(Client client) {
         client.callingClient = this;
@@ -28,5 +38,63 @@ public class Client {
 
     public boolean isConnected(Client client) {
         return client == connectedClient;
+    }
+
+    public void sendFrame() throws IOException {
+        if (connectedClient == null) return;
+
+        byte[] imageData = getImageData();
+        sendImage(imageData);
+
+        connectedClient.onFrame();
+    }
+
+    public void onFrame() {
+        receivedFrames++;
+    }
+
+    private static void sendImage(byte[] imageData) throws IOException {
+        try (DatagramSocket socket = new DatagramSocket()) {
+
+            // Send the image data
+            String receiverHost = "localhost";
+            int receiverPort = 9001;
+            InetAddress receiverAddress = InetAddress.getByName(receiverHost);
+
+            int maxPacketSize = 1460;
+            int offset = 0;
+
+            while (offset < imageData.length) {
+                int size = Math.min(maxPacketSize, imageData.length - offset);
+                byte[] packetData = new byte[size];
+
+                System.arraycopy(imageData, offset, packetData, 0, size);
+
+                DatagramPacket packet = new DatagramPacket(packetData, size, receiverAddress, receiverPort);
+                socket.send(packet);
+
+                offset += size;
+            }
+        }
+    }
+
+    private static byte[] getImageData() throws IOException {
+        byte[] imageData;
+        Robot robot = null;
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            throw new RuntimeException(e);
+        }
+
+        Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+        BufferedImage screenshot = robot.createScreenCapture(screenRect);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(screenshot, "jpg", baos);
+        baos.flush();
+        imageData = baos.toByteArray();
+        baos.close();
+        return imageData;
     }
 }
--- src/test/java/TestImageSending.java
+++ src/test/java/TestImageSending.java
@@ -1,7 +1,8 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestImageSending {
     @Test
@@ -62,5 +63,21 @@ public class TestImageSending {
         client2.decline(client1);
 
         assertTrue(client2.isConnected(client1));
+    }
+
+    @Test
+    void clientCanSendAndReceiveFrame() {
+        var client1 = new Client();
+        var client2 = new Client();
+
+        client1.call(client2);
+        client2.accept(client1);
+
+        try {
+            client1.sendFrame();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(1, client2.receivedFrames);
     }
 }
